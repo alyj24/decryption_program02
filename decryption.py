82eaa7e6ff3559b3f147ed17ed94007cf61e0516
Family <albon.136546120855@2depedqc.ph>
2023-04-20 23:36:33 +0800

    Pseudocodes

--- decryption.py
+++ decryption.py
@@ -1,1 +1,12 @@
 print("Problem 02 ~ Assignment 2".center(42,"="))
+
+# pseudocode
+# ask the user for an input and save its input
+# recognize each character of the input
+# if *, change it to a
+# if &, change it to e
+# if #, change it to i
+# if +, change it to o
+# if !, change it to u
+# print the output
+
