67a0f7ff54e6bcfc667aae653947f5152b34753d
Family <albon.136546120855@2depedqc.ph>
2023-04-20 23:39:54 +0800

    Input is working, Great!

--- decryption.py
+++ decryption.py
@@ -2,6 +2,7 @@ print("Problem 02 ~ Assignment 2".center(42,"="))
 
 # pseudocode
 # ask the user for an input and save its input
+input_str = input("\033[91mGood Day! Please enter your input string: ")
 # recognize each character of the input
 # if *, change it to a
 # if &, change it to e
